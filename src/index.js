41081ed8e374ee40d365447e31d257ad152e1577
Dan Lynch <pyramation@gmail.com>
2022-10-20 16:22:15 -0700

    upgrades

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ module.exports = () => {
       ['@babel/transform-runtime', { useESModules }],
       '@babel/proposal-object-rest-spread',
       '@babel/proposal-class-properties',
-      '@babel/proposal-export-default-from',
-      'macros',
+      '@babel/proposal-export-default-from'
     ],
     presets: useESModules ? ['@babel/react'] : ['@babel/env', '@babel/react']
   };
